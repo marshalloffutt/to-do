023651c8c35aa4a31886644cd9cd3fe29c5de42d
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-15 19:07:40 -0600

    links all work

--- src/helpers/authHelpers.js
+++ src/helpers/authHelpers.js
@@ -1,27 +1,27 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import $ from 'jquery';
 
-// const checkLoginStatus = () => {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     if (user) {
-//       $('#tasks').show();
-//       $('#done').hide();
-//       $('#auth').hide();
-//       $('#navbar-button-auth').hide();
-//       $('#navbar-button-tasks').show();
-//       $('#navbar-button-done').show();
-//       $('#navbar-button-logout').show();
-//     } else {
-//       $('#tasks').hide();
-//       $('#done').hide();
-//       $('#auth').show();
-//       $('#navbar-button-auth').show();
-//       $('#navbar-button-tasks').hide();
-//       $('#navbar-button-done').hide();
-//       $('#navbar-button-logout').hide();
-//     }
-//   });
-// };
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      $('#tasks').show();
+      $('#done').hide();
+      $('#auth').hide();
+      $('#navbar-button-auth').hide();
+      $('#navbar-button-tasks').show();
+      $('#navbar-button-done').show();
+      $('#navbar-button-logout').show();
+    } else {
+      $('#tasks').hide();
+      $('#done').hide();
+      $('#auth').show();
+      $('#navbar-button-auth').hide();
+      $('#navbar-button-tasks').hide();
+      $('#navbar-button-done').hide();
+      $('#navbar-button-logout').hide();
+    }
+  });
+};
 
-// export default checkLoginStatus;
+export default checkLoginStatus;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import apiKeys from '../db/apiKeys.json';
 import './index.scss';
 import createNavbar from './components/navbar/navbar';
 import loginButton from './components/auth/auth';
-// import checkLoginStatus from './helpers/authHelpers';
+import checkLoginStatus from './helpers/authHelpers';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
-  // checkLoginStatus();
+  checkLoginStatus();
   loginButton();
 };
 
