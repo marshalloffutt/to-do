ecacc69e557b44a2de8ef82979d27407b1d3c023
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-15 18:43:55 -0600

    Adds google login auth

---
+++ src/components/auth/auth.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import './auth.scss';
+import loginImage from './signin.png';
+
+const loginButton = () => {
+  const domString = `
+    <button id="google-auth" class="btn btn-secondary">
+      <img src="${loginImage}"/>
+    </button>
+  `;
+  $('#auth').html(domString);
+  $('#google-auth').on('click', () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  });
+};
+
+export default loginButton;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from '../db/apiKeys.json';
 import './index.scss';
-// import $ from 'jquery';
 import createNavbar from './components/navbar/navbar';
+import loginButton from './components/auth/auth';
+// import checkLoginStatus from './helpers/authHelpers';
 
 const initializeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  // checkLoginStatus();
+  loginButton();
 };
 
 initializeApp();
